3f0ffd2248cf9b9a55505b8615217a2763086adb
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-12-22 00:03:11 +0200

    Remove prefill

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@ for T in 1 2 4 16 32 128
 do
 	for K in 256 2560 25600 256000 2560000 25600000 256000000
 	do
-		./run $T 10 10 -keys$K -ins40 -del40 -prefill
 		./run $T 10 10 -keys$K -ins40 -del40
 	done
 done
