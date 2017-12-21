e835d60d166e8386049557cdad4f7d893a27161e
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-12-21 23:47:40 +0200

    Fix whitespace

--- src/algorithms/BST.java
+++ src/algorithms/BST.java
@@ -5,7 +5,7 @@ import main.BSTInterface;
 public class BST implements BSTInterface {
 	static enum Direction {
 		LEFT, RIGHT;
-		
+
 		public static Direction next(final int current, final int target) {
 			if (current < target) {
 				return Direction.RIGHT;
@@ -16,17 +16,17 @@ public class BST implements BSTInterface {
 			}
 		}
 	}
-	
+
 	static class Node {
 		public final int key;
 		public volatile Node left;
 		public volatile Node right;
 		public volatile boolean marked;
-		
+
 		Node(int key) {
 			this.key = key;
 		}
-		
+
 		public final Node get(final Direction dir) {
 			switch (dir) {
 			case LEFT:
@@ -36,8 +36,8 @@ public class BST implements BSTInterface {
 			default:
 				throw new RuntimeException(dir.toString());
 			}
-		}	
-		
+		}
+
 		public final void set(final Direction dir, final Node node) {
 			switch (dir) {
 			case LEFT:
@@ -54,12 +54,12 @@ public class BST implements BSTInterface {
 				throw new RuntimeException(dir.toString());
 			}
 		}
-		
+
 		public String toString() {
 			return String.format("Node(key=%d)", key);
 		}
 	}
-	
+
 	private final Node root;
 
     public BST() {
@@ -68,25 +68,25 @@ public class BST implements BSTInterface {
 
     public final boolean contains(final int key) {
     	Node curr = root;
-    	
+
     	while (curr != null && curr.key != key) {
     		curr = curr.get(Direction.next(curr.key, key));
     	}
-    	
+
     	return curr != null && !curr.marked && curr.key == key;
     }
 
     public final boolean insert(final int key) {
     	while (true) {
     		Node curr = root;
-    		
+
     		while (curr.key != key) {
     			Node next = curr.get(Direction.next(curr.key, key));
     			if (next == null)
     				break;
     			curr = next;
     		}
-    		
+
     		synchronized (curr) {
     			if (!curr.marked) {
     				if (curr.key == key)
@@ -94,7 +94,7 @@ public class BST implements BSTInterface {
     				Direction dir = Direction.next(curr.key, key);
     				if (curr.get(dir) == null) {
     					// Perform extra traversal, in case we're inserting value to a recent successor node.
-    					Node sanity = root;    		
+    					Node sanity = root;
     					while (sanity.key != key) {
     		    			Node next = sanity.get(Direction.next(sanity.key, key));
     		    			if (next == null)
@@ -104,14 +104,14 @@ public class BST implements BSTInterface {
     					if (sanity == curr) {
     						Node node = new Node(key);
         					curr.set(dir, node);
-        					return true;    						
+        					return true;
     					}
     				}
     			}
 			}
     	}
     }
-    
+
     private final boolean validate(final Node parent, final Node child) {
     	return !parent.marked &&
     			!child.marked &&
@@ -123,7 +123,7 @@ public class BST implements BSTInterface {
     	while (true) {
     		Node pred = null;
     		Node curr = root;
-    		
+
     		while (curr.key != key) {
     			Node next = curr.get(Direction.next(curr.key, key));
     			if (next == null)
@@ -131,27 +131,27 @@ public class BST implements BSTInterface {
     			pred = curr;
     			curr = next;
     		}
-    		
+
     		synchronized (pred) {
     			synchronized (curr) {
     				Direction curr_dir = Direction.next(pred.key, curr.key);
     				if (validate(pred, curr)) {
     					final Node left = curr.left;
     					final Node right = curr.right;
-    					
+
     					if (left != null && right != null) {
     						synchronized (left) {
     							synchronized (right) {
     	    						Node succ_pred = curr;
     	    						Node succ = right;
     	    						Node next = succ.left;
-    	    						
+
     	    						while (next != null) {
     	    							succ_pred = succ;
     	    							succ = next;
     	    							next = next.left;
     	    						}
-    	    						
+
     	    						synchronized (succ_pred) {
     	    							synchronized (succ) {
     										if (validate(succ_pred, succ) && succ.left == null) {
@@ -161,7 +161,7 @@ public class BST implements BSTInterface {
     												pred.set(curr_dir, right);
     												return true;
     											} else {
-    												final Node replacement = new Node(succ.key);    												
+    												final Node replacement = new Node(succ.key);
     												replacement.left = left;
     												replacement.right = right;
     												curr.marked = true;
@@ -208,7 +208,7 @@ public class BST implements BSTInterface {
     public final int size() {
     	return size(root.right);
     }
-    
+
     private final int size(final Node node) {
     	if (node == null)
     		return 0;
@@ -220,11 +220,11 @@ public class BST implements BSTInterface {
     public final long getKeysum() {
     	return getKeysum(root.right);
     }
-    
+
     private final long getKeysum(final Node node) {
-    	if (node == null) 
+    	if (node == null)
     		return 0;
-    	
+
     	return node.key + getKeysum(node.left) + getKeysum(node.right);
     }
 }
