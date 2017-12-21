7ed1db53cb2e4de9beeabd9f83c1e17507af8c64
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-12-22 00:02:08 +0200

    Un-dummy the tester

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ for T in 1 2 4 16 32 128
 do
 	for K in 256 2560 25600 256000 2560000 25600000 256000000
 	do
-		echo ./run $T 10 60 -keys$K -ins40 -del40 -prefill
-		echo ./run $T 10 60 -keys$K -ins40 -del40
+		./run $T 10 10 -keys$K -ins40 -del40 -prefill
+		./run $T 10 10 -keys$K -ins40 -del40
 	done
 done
