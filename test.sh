8e19155a29213a9627dbc0284e715931020c79b6
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-12-22 00:05:42 +0200

    Parametrize INS/DEL

--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@ for T in 1 2 4 16 32 128
 do
 	for K in 256 2560 25600 256000 2560000 25600000 256000000
 	do
-		./run $T 10 10 -keys$K -ins40 -del40
+		for INS in 70 50 40 30 10
+		do
+			export DEL=$(( 80 - $INS ))
+			./run $T 10 10 -keys$K -ins$INS -del$DEL
+		done
 	done
 done
