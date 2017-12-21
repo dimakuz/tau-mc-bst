0f1efbae9dd20f799ba59a2be296e891afe3c86d
Dima Kuznetsov <dima.kuznetsov@toganetworks.com>
2017-12-21 19:55:17 +0200

    Initial commit

---
+++ src/algorithms/BST.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+import main.BSTInterface;
+
+public class BST implements BSTInterface {
+
+    public BST() {
+    }
+
+    public final boolean contains(final int key) {
+    }
+
+    public final boolean insert(final int key) {
+    }
+
+    public final boolean remove(final int key) {
+    }
+
+    // Return your ID #
+    public String getName() {
+        return "XXXXXXXXXX";
+    }
+
+    // Returns size of the tree.
+    // NOTE: Guaranteed to be called without concurrent operations.
+    public final int size() {
+    }
+
+    // Returns the sum of keys in the tree
+    // NOTE: Guaranteed to be called without concurrent operations.
+    public final long getKeysum() {
+    }
+}
